75d0545456735e3460f12888367176395cbbd5d4
joydeep-git <joydeep.accounts@tuta.io>
2025-01-18 12:56:12 +0530

    updated authenticated layout component logic

--- src/app/(authenticated_routes)/layout.tsx
+++ src/app/(authenticated_routes)/layout.tsx
@@ -1,12 +1,22 @@
 "use client";
 
 import React, { ReactNode } from 'react';
+import { useAppSelector } from '@/redux/store';
+import { useRouter } from 'next/navigation';
 
-const AuthenticatedLayout = ({children}: {children: ReactNode}) => {
 
-  return (
-    <div>{children}</div>
-  )
+const AuthenticatedLayout = ({ children }: { children: ReactNode }) => {
+
+  const router = useRouter();
+
+  // States
+  const { user } = useAppSelector(state => state.auth);
+
+  if (user) {
+    return <>{children}</>
+  } else {
+    router.push("/sign-in")
+  }
 }
 
 export default AuthenticatedLayout;
