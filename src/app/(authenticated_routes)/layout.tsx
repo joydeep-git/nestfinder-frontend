412c8b11d09e29331ff64e851599fdb34fffaa7f
joydeep-git <joydeep.accounts@tuta.io>
2025-01-28 19:55:16 +0530

    changed authenticated route layout structure

--- src/app/(authenticated_routes)/layout.tsx
+++ src/app/(authenticated_routes)/layout.tsx
@@ -1,22 +1,28 @@
 "use client";
 
-import React, { ReactNode } from 'react';
-import { useAppSelector } from '@/redux/store';
-import { useRouter } from 'next/navigation';
-
+import React, { ReactNode, useEffect } from "react";
+import { useAppSelector } from "@/redux/store";
+import { useRouter } from "next/navigation";
+import LoadingAnimation from "@/components/utils/LoadingAnimation";
 
 const AuthenticatedLayout = ({ children }: { children: ReactNode }) => {
-
   const router = useRouter();
 
-  // States
-  const { user } = useAppSelector(state => state.auth);
+  // states
+  const { user } = useAppSelector((state) => state.auth);
 
-  if (user) {
-    return <>{children}</>
-  } else {
-    router.push("/sign-in")
+  useEffect(() => {
+    if (!user) {
+      router.push("/sign-in");
+    }
+  }, [user, router]);
+
+  
+  if (!user) {
+    return <LoadingAnimation />;
   }
-}
+
+  return <>{children}</>;
+};
 
 export default AuthenticatedLayout;
