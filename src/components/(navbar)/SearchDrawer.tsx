89a9440b3c457f48b4e38522d3e01ce72fc7116b
joydeep-git <joydeep.accounts@tuta.io>
2025-01-29 18:45:29 +0530

    search drawer component WIP

---
+++ src/components/(navbar)/SearchDrawer.tsx
@@ -0,0 +1,31 @@
+import { Button } from '@/components/ui/button';
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from '@/components/ui/drawer';
+import { Search } from 'lucide-react';
+
+const SearchModal = () => {
+  return (
+    <Drawer direction='top'>
+      <DrawerTrigger asChild>
+        <Search className='h-5 w-5 cursor-pointer hover:text-orange-500' strokeWidth={2} />
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Move Goal</DrawerTitle>
+          <DrawerDescription>Set your daily activity goal.</DrawerDescription>
+        </DrawerHeader>
+
+
+
+
+        <DrawerFooter>
+          <Button>Submit</Button>
+          <DrawerClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default SearchModal;
