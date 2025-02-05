b9ddf1272f5dc30bbb15a7c4b4f9d31b12f7d28d
joydeep-git <joydeep.accounts@tuta.io>
2025-02-06 01:15:55 +0530

    changes in checkbox component for  better usability

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -13,9 +13,13 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
+      "peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
       className
     )}
+    onCheckedChange={(checked) => {
+      console.log("Checkbox Checked: ", checked);
+      props.onCheckedChange?.(checked);
+    }}
     {...props}
   >
     <CheckboxPrimitive.Indicator
@@ -28,3 +32,4 @@ const Checkbox = React.forwardRef<
 Checkbox.displayName = CheckboxPrimitive.Root.displayName
 
 export { Checkbox }
+
