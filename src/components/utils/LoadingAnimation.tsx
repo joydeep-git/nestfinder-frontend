b9314b5b232f561816bfd6c5a89828d6c6b0ebd7
joydeep-git <joydeep.accounts@tuta.io>
2025-03-15 09:24:29 +0530

    changed loading animation

--- src/components/utils/LoadingAnimation.tsx
+++ src/components/utils/LoadingAnimation.tsx
@@ -3,7 +3,14 @@ import React from 'react';
 const LoadingAnimation = () => {
 
   return (
-    <div className="loadingAnimation"></div>
+    <div className="spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
   )
 }
 
