924fdc0a6949f6aed7743bd084347163338dadc8
joydeep-git <joydeep.accounts@tuta.io>
2025-03-18 15:18:44 +0530

    display message when no property available on  products route

--- src/app/(unauthenticated_routes)/products/page.tsx
+++ src/app/(unauthenticated_routes)/products/page.tsx
@@ -3,15 +3,18 @@
 import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { productService } from '@/services/productService';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import ProductCard from '@/components/ProductCard';
-import { MotionLayoutDiv } from '@/components/utils/motionWrapper';
+import { MotionLayoutDiv, MotionText } from '@/components/utils/motionWrapper';
 import LoadingAnimation from '@/components/utils/LoadingAnimation';
+import { Button } from '@/components/ui/button';
+import { RefreshCcw } from 'lucide-react';
 
 const Products = () => {
 
   // get search params
   const searchParams = useSearchParams();
+  const router = useRouter();
 
   // fetch products
   const { data, isLoading } = useQuery({
@@ -21,12 +24,23 @@ const Products = () => {
   });
 
 
-  if(isLoading) return <LoadingAnimation />
+  if (isLoading) return <LoadingAnimation />
 
   return (
     <MotionLayoutDiv className='items-center md:items-start justify-start flex-wrap flex-row w-full flex-1 mx-auto flex gap-6'>
       {
-        data?.data.map((product) => { return (<ProductCard property={product} key={product._id} /> ) } )
+        (data?.data && data?.data.length > 0)
+          ? data?.data.map((product) => { return (<ProductCard property={product} key={product._id} />) })
+          : (
+            <div className='flex-1 pt-16'>
+              <MotionText className='flex flex-col gap-6 text-center items-center'>
+                <span>No Property available!</span>
+                <Button className='w-fit' size={'sm'} onClick={() => router.push("/products")}>
+                  Reset Filter <RefreshCcw />
+                </Button>
+              </MotionText>
+            </div>
+          )
       }
     </MotionLayoutDiv>
   )
