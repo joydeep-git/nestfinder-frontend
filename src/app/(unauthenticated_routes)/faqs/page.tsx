d419d6c5373af994d23760bdf6b751968a12fc71
joydeep-git <joydeep.accounts@tuta.io>
2025-03-24 12:44:34 +0530

    added text-left on faq tiggers

--- src/app/(unauthenticated_routes)/faqs/page.tsx
+++ src/app/(unauthenticated_routes)/faqs/page.tsx
@@ -10,35 +10,35 @@ const Page = () => {
       <Accordion type="single" collapsible className="w-full">
 
         <AccordionItem value="zero-brokerage">
-          <AccordionTrigger>Is Nest Finder a zero-brokerage platform?</AccordionTrigger>
+          <AccordionTrigger className="text-left">Is Nest Finder a zero-brokerage platform?</AccordionTrigger>
           <AccordionContent>
             Yes! Nest Finder ensures **zero brokerage** for buyers and tenants. We connect you directly to property owners or dealers.
           </AccordionContent>
         </AccordionItem>
 
         <AccordionItem value="connects-buyers">
-          <AccordionTrigger>How does Nest Finder connect buyers with dealers?</AccordionTrigger>
+          <AccordionTrigger className="text-left">How does Nest Finder connect buyers with dealers?</AccordionTrigger>
           <AccordionContent>
             We provide a **direct connection** between buyers and verified dealers, ensuring a smooth and hassle-free property transaction.
           </AccordionContent>
         </AccordionItem>
 
         <AccordionItem value="list-properties">
-          <AccordionTrigger>Can I list my property for free?</AccordionTrigger>
+          <AccordionTrigger className="text-left">Can I list my property for free?</AccordionTrigger>
           <AccordionContent>
             Yes! You can list your property **for free** on Nest Finder and reach thousands of potential buyers and tenants.
           </AccordionContent>
         </AccordionItem>
 
         <AccordionItem value="verified-listings">
-          <AccordionTrigger>Are the property listings verified?</AccordionTrigger>
+          <AccordionTrigger className="text-left">Are the property listings verified?</AccordionTrigger>
           <AccordionContent>
             Absolutely! Our team verifies each listing to ensure authenticity, so you can **trust every property** on our platform.
           </AccordionContent>
         </AccordionItem>
 
         <AccordionItem value="support">
-          <AccordionTrigger>How can I contact support?</AccordionTrigger>
+          <AccordionTrigger className="text-left">How can I contact support?</AccordionTrigger>
           <AccordionContent>
             You can reach out to our **24/7 customer support** via email at joydeepdas@zohomail.com or call us at +91-6290589624.
           </AccordionContent>
